41634bc975ce70fa294bde06ca9c23672943a45b
Gionata Massi (in qualità di docente d'Informatica) <gionata.massi@savoiabenincasa.it>
2022-10-25 13:36:29 +0200

    Aggiunti esempi del 25 ottobre

---
+++ 3asia/2022-10-25/campanella.cpp
@@ -0,0 +1,11 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cout << "\a" << endl;
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
---
+++ 3asia/2022-10-25/crypto_1.cpp
@@ -0,0 +1,13 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    char bandiera[] = {102, 108, 97, 103, 123, 117, 103, 104, 95, 78, 117, 109, 66, 51, 114, 53, 95, 52, 49, 114, 51, 52, 100, 121, 125};
+    cout << bandiera << endl;
+    
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
