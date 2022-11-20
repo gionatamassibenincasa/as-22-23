0f0926662e2f56557dadda33382d0b644eaf26b6
Gionata Massi <gionata.massi@savoiabenincasa.it>
2022-11-20 16:52:24 +0100

    Modificato generatore JSON con appunti su operazioni mancanti

--- quiz/genera_ast_quizzes.js
+++ quiz/genera_ast_quizzes.js
@@ -33,6 +33,8 @@ files.forEach((f) => {
       }
     });
     quizzes.push({
+      baseDir: f.substring(2, f.lastIndexOf("/js")),
+      // httpsImageDir: f.substring(0, f.lastIndexOf("/js")) + "/images",
       classe: vol,
       capitolo: cap,
       titolo: options.title,
@@ -44,4 +46,42 @@ files.forEach((f) => {
     exit(1);
   }
 });
+
+const imageSrcAndWidthRE = new RegExp(
+  "<br\\s*/> <img src='(images/.*.png)'\\s*width='([0-9]+)'\\s*/>"
+);
+
+quizzes.forEach((quiz) => {
+  Object.keys(quiz.quizMulti).forEach((k) => {
+    quiz.quizMulti[k].forEach((quizStructure) => {
+      if (quizStructure.ques.indexOf("<img src='") != -1) {
+        let matches = quizStructure.ques.match(imageSrcAndWidthRE);
+        const path = quiz.baseDir + "/" + matches[1];
+        // const buffer = fs.readFileSync(path);
+        //quizStructure.image = buffer.toString("base64");
+        quizStructure.url = `https://github.com/gionatamassibenincasa/as-22-23/raw/main/quiz/${path}/`;
+      }
+    });
+  });
+});
+
+// TODO:
+// Per ogni tipo di domanda, su ques, ans e ansSel:
+//   1. Rimpiazzare <br> con \n
+//   2. Convertire le entità: &agrave; &eacute; &egrave; &ograve; &ugrave; &ndash; &lt; &gt; ...
+//   3. Eliminare i tag pre.
+//   4. Eliminare tag rimanenti
+
+quizzes.forEach((q) => {
+  for (t of Object.keys(q.quizMulti)) {
+    let quizGroupArray = q.quizMulti[t];
+    quizGroupArray.forEach((item) => {
+      for (stringKey of Object.keys(item)) {
+        let txt;
+        console.log(stringKey);
+      }
+    });
+  }
+});
+return 0;
 console.log(JSON.stringify(quizzes, null, 2));
