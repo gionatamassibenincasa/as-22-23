20f1d8ef857f677e42ae820dfbafa3d5f60a145c
Gionata Massi <gionata.massi@savoiabenincasa.it>
2023-01-26 14:40:05 +0100

    Correzione dell'esercitazione vol. 3 p. 127

--- 3asia/2023-01-20/esercizi_p127_1_12.cpp
+++ 3asia/2023-01-20/esercizi_p127_1_12.cpp
@@ -25,7 +25,7 @@ void es_03(void) {
   double r, P, A;
   cout << "Immetti il raggio del cerchio: ";
   cin >> r;
-  P = r * M_PI;
+  P = 2 * M_PI * r;
   A = r * r * M_PI;
   cout << "P: " << P << endl;
   cout << "A: " << A << endl;
@@ -51,13 +51,91 @@ void es_05(void) {
   ip = sqrt(c1 * c1 + c2 * c2);
   cout << "Ipotenusa: " << ip << endl;
 }
-void es_06(void) {}
-void es_07(void) {}
-void es_08(void) {}
-void es_09(void) {}
-void es_10(void) {}
-void es_11(void) {}
-void es_12(void) {}
+void es_06(void) {
+  int e1, e2, e3;
+  double somma, media;
+  cout << "Immetti l'eta' della persona 1: ";
+  cin >> e1;
+  cout << "Immetti l'eta' della persona 2: ";
+  cin >> e2;
+  cout << "Immetti l'eta' della persona 3: ";
+  cin >> e3;
+  somma = e1 + e2 + e3;
+  media = somma / 3;
+  cout << "Eta' media: " << media << endl;
+}
+void es_07(void) {
+  // I numeri negativi fanno produrre:
+  // Not A Number (NAN)
+  cout << "Immetti x: ";
+  int x;
+  float y;
+  cin >> x;
+  y = sqrt(x);
+  cout << "Radice quadrata  di " << x << ": " << y << endl;
+}
+void es_08(void) {
+  cout << "Riga 1: #include<iostream>" << endl;
+  cout << "Riga 4: /* Input */ oppure // Input" << endl;
+  cout << "Riga 6: Mancano le virgolette" << endl;
+  cout << "Riga 9: cout" << endl << endl;
+
+  int i;
+  cout << "Introduci un numero: ";
+  cin >> i;
+  i *= 2;
+  cout << i;
+
+  cout << endl;
+}
+void es_09(void) {
+  double x1, y1, x2, y2, xm, ym;
+  cout << "Immetti le coordinate di P1 separatate da uno spazio: ";
+  cin >> x1 >> y1;
+  cout << "Immetti le coordinate di P2 separatate da uno spazio: ";
+  cin >> x2 >> y2;
+  xm = (x1 + x2) / 2.0;
+  ym = (y1 + y2) / 2.0;
+  cout << "Il punto medio tra i due dati è (" << xm << ", " << ym << ")"
+       << endl;
+}
+void es_10(void) {
+  double x1, y1, x2, y2, m, q;
+  cout << "Immetti le coordinate di P1 separatate da uno spazio: ";
+  cin >> x1 >> y1;
+  cout << "Immetti le coordinate di P2 separatate da uno spazio: ";
+  cin >> x2 >> y2;
+  m = (y2 - y1) / (x2 - x1);
+  q = y1 - m * x1;
+  cout << "y = ";
+  if (m != 1)
+    cout << m << " ";
+  cout << "x";
+  if (q > 0)
+    cout << " + " << q;
+  else if (q < 0)
+    cout << " - " << (-q);
+  cout << endl;
+}
+void es_11(void) {
+  double prezzoListino, percSconto, prezzo;
+  cout << "Prezzo di listino: ";
+  cin >> prezzoListino;
+  cout << "Percentuale sconto: ";
+  cin >> percSconto;
+  prezzo = prezzoListino * (1 - percSconto / 100);
+  cout << "Prezzo scontato: " << prezzo << endl;
+}
+void es_12(void) {
+  double capitale, tassoInteresseAnnuale, giorni, interesseMaturato;
+  cout << "Capitale: ";
+  cin >> capitale;
+  cout << "Tasso di interesse annuale: ";
+  cin >> tassoInteresseAnnuale;
+  giorni = 25;
+  interesseMaturato = capitale * tassoInteresseAnnuale * giorni / 365 / 100;
+  cout << "Interesse maturato: " << interesseMaturato << endl;
+}
 
 int main() {
   struct esercizio_t {
