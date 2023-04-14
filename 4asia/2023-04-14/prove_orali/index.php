0486b0098a6cd1cd2c2bb1f1b8a7c8a72d44b874
Gionata Massi <gionata.massi@savoiabenincasa.it>
2023-04-14 13:45:56 +0200

    Modificato esempio con MaterialDesignLite

--- 4asia/2023-04-14/prove_orali/index.php
+++ 4asia/2023-04-14/prove_orali/index.php
@@ -1,6 +1,6 @@
 <?php
   $db = new PDO('sqlite:registro.sqlite3');
-  $qry = $db->query("SELECT classeId, anno || ' ' || sezione || ' ' || articolazione AS classe FROM Classe");
+  $qry = $db->query("SELECT classeId, anno || ' ' || sezione || ' ' || articolazione AS classe FROM Classe ORDER BY classe");
   $classi = $qry->fetchAll();
 ?>
 
@@ -8,19 +8,45 @@
 <html lang="it">
 	<head>
 		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+		<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:300,400,500,700" type="text/css">
+		<link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-blue.min.css">
+		<script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script> 
+
 		<title>Prove orali - Scelta classe</title>
 	</head>
 	<body>
-		<form action="appello.php" method="GET">
-			<label>Classe:
-				<select name="classeId">
+    <div>
+  <!-- Always shows a header, even in smaller screens. -->
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+  <header class="mdl-layout__header">
+    <div class="mdl-layout__header-row">
+      <!-- Title -->
+      <span class="mdl-layout-title">Registro del docente</span>
+      <!-- Add spacer, to align navigation to the right -->
+      <div class="mdl-layout-spacer"></div>
+      <!-- Navigation. We hide it in small screens. -->
+      <nav class="mdl-navigation mdl-layout--large-screen-only">
+        <a class="mdl-navigation__link" href="/index.php">Appello</a>
+      </nav>
+    </div>
+  </header>
+  <div class="mdl-layout__drawer">
+    <span class="mdl-layout-title">Scegli la classe</span>
+    <nav class="mdl-navigation">
           <?php
-              for($i = 0; $i < count($classi); $i++)
-                echo '<option value="' . $classi[$i]['classeId'] . '">' . $classi[$i]['classe'] . '</option>';
+                  for($i = 0; $i < count($classi); $i++) {
+                    echo '<a class="mdl-navigation__link" href="/appello.php?classeId=' . $classi[$i]['classeId'] . '">' . $classi[$i]['classe'] . '</a>' . "\n";
+                  }
           ?>
-				</select>
-			</label>
-			<button type="submit">Fai l'appello</button>
-		</form>
+    </nav>
+  </div>
+  <main class="mdl-layout__content">
+    <div class="page-content">
+      <!-- Carica dinamicamente la pagina opportuna (appello, interrogati, domande) -->
+    </div>
+  </main>
+    </div>
 	</body>
 </html>
